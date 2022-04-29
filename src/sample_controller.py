44a76fe3724bba93b1378e000604fd7c5804d3bf
Max J <98533736+margens1@users.noreply.github.com>
2022-04-29 21:29:10 +0000

    Worked in menuloop()

--- src/sample_controller.py
+++ src/sample_controller.py
@@ -1,5 +1,11 @@
+import pygame
+import sys
+
 
 class Controller:
+
+  screen = pygame.display.set_mode((500, 500),0,32)
+  font = pygame.font.SysFont(None, 20)
   
   def __init__(self):
     #setup pygame data
@@ -11,7 +17,28 @@ class Controller:
   ### below are some sample loop states ###
 
   def menuloop(self):
-    
+    while True:
+
+      screen.fill((0,0,0))    ## makes the entire screen black
+      draw_text('Falling Frenzy', font, (255, 255, 255), screen, 20, 20)  #writes falling frenzy
+
+      EasyMode = pygame.Rect(50, 75, 200, 50)     ##Button for easy mode
+      MediumMode = pygame.Rect(50, 150, 200, 50)  ##Button for medium mode
+      HardMode = pygame.Rect(50, 225, 200, 50)    ##Button for hard mode
+
+
+      if EasyMode.collidepoint((mx, my)):
+        if click:
+          ##Play game with easy difficulty
+      if MediumMode.collidepoint((mx, my)):
+        if click:
+          ##Play game with medium difficulty
+      if HardMode.collidepoint((mx, my)):
+        if click:
+          ##Play game with hard difficulty
+      pygame.draw.rect(screen, (255, 0, 0), EasyMode)
+      pygame.draw.rect(screen, (255, 0, 0), MediumMode)
+      pygame.draw.rect(screen, (255, 0, 0), HardMode)
       #event loop
 
       #update data
@@ -19,6 +46,7 @@ class Controller:
       #redraw
       
   def gameloop(self):
+    running = True
       #event loop
 
       #update data
