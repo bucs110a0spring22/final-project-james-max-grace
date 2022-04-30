32fd8f37f8d7f03fac711a136aec8d1bd9f0b102
Jamesb27598 <98533827+JamesB275@users.noreply.github.com>
2022-04-30 22:28:41 +0000

    Added to Collision to make it decrease player health by 1/3

--- src/player.py
+++ src/player.py
@@ -7,6 +7,7 @@ class Player(pygame.sprite.Sprite):
 	def __init__(self, x=0, y=0, filenames=None):
 		super()
 		self.image_set = [pygame.image.load(file_name) for f in filenames]
+    self.player_health=100
 		self.current_image=0
     self.image = self.image_set[self.current_image] 
     self.rect = self.image.get_rect() 
@@ -15,7 +16,9 @@ class Player(pygame.sprite.Sprite):
 
   def collison(self, fallingobject):
     if pygame.sprite.spritecollideany(Player, FallingObjects):
-      playerHealth()
+      player_health-=33
+  if player_health==0:
+   death()
 	
 	def move(self, direction):
 		if direction == "left":
@@ -26,10 +29,8 @@ class Player(pygame.sprite.Sprite):
             self.rect.y = self.rect.y - 1 
     elif direction == "down":
             self.rect.y = self.rect.y + 1
-  
-	def playerHealth(self):
-	#player loses 1/3 health when collision occurs
-
+   
+  def death():
 
 
 
