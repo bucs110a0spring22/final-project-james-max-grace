09b745db3279c0f897871fc71e1b790f82fb01c1
Max J <98533736+margens1@users.noreply.github.com>
2022-05-06 17:47:47 +0000

    Controller changes

--- src/controller.py
+++ src/controller.py
@@ -1,79 +1,86 @@
-import pygame
-import sys
-
-
-class Controller:
-
-  screen = pygame.display.set_mode((500, 500),0,32) ##sets the screen
-  font = pygame.font.SysFont(None, 20) ##sets the font of the writing
-  
-  def __init__(self):
-    pygame.init()
-    self.screen = pygame.display.set_mode((0, 0))
-    
-    
-    #setup pygame data
-    
-  def mainloop(self):
-    ##unfinished
-    while True:      
-      if(self.state == "GAME"):
-        self.gameLoop()
-      elif(self.state == "GAMEOVER"):
-        self.gameOver()
-  
-  def menuloop(self):
-        ##unfinished
+import pygame, sys
+ 
+mainClock = pygame.time.Clock()
+from pygame.locals import *
+pygame.init()
+pygame.display.set_caption('game base')
+screen = pygame.display.set_mode((500, 500),0,32)
+ 
+font = pygame.font.SysFont(None, 75)
+font2 = pygame.font.SysFont(None, 40)
+ 
+def draw_text(text, font, color, surface, x, y):
+    textobj = font.render(text, 1, color)
+    textrect = textobj.get_rect()
+    textrect.topleft = (x, y)
+    surface.blit(textobj, textrect)
+ 
+click = False
+ 
+def main_menu():
     while True:
+ 
+        screen.fill((150, 150, 250))
+        draw_text('Falling Frenzy', font, (255, 255, 255), screen, 90, 100)
+        draw_text('Start', font2, (0,0,0), screen, 250, 300)
+ 
+        mx, my = pygame.mouse.get_pos()
+ 
+        button_1 = pygame.Rect(150, 250, 200, 75)
 
-      screen.fill((0,0,0))    ## makes the entire screen black
-      draw_text('Falling Frenzy', font, (255, 255, 255), screen, 20, 20)  #writes falling frenzy
-'''
-      EasyMode = pygame.Rect(50, 75, 200, 50)     ##Button for easy mode
-      MediumMode = pygame.Rect(50, 150, 200, 50)  ##Button for medium mode
-      HardMode = pygame.Rect(50, 225, 200, 50)    ##Button for hard mode
-'''
-
-      if EasyMode.collidepoint((mx, my)):
-        if click:
-          ##Play game with easy difficulty
-      if MediumMode.collidepoint((mx, my)):
-        if click:
-          ##Play game with medium difficulty
-      if HardMode.collidepoint((mx, my)):
-        if click:
-          ##Play game with hard difficulty
-          '''
-      pygame.draw.rect(screen, (255, 0, 0), EasyMode)
-      pygame.draw.rect(screen, (255, 0, 0), MediumMode)
-      pygame.draw.rect(screen, (255, 0, 0), HardMode)
-    '''
-      
-  def gameloop(self):    ##add timer - top left
-        ##unfinished
-    while self.state == "GAME":         
-      for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-          sys.exit()
-        if event.type == pygame.KEYDOWN:
-          if (event.key == pygame.K_LEFT):
-            self.hero.move_left()
-          elif(event.key == pygame.K_RIGHT):
-            self.hero.move_right()
-    
-  def gameoverloop(self):    ##add points count
-    ##unfinished
-      self.hero.kill()   ##remove sprite
-        myfont = pygame.font.SysFont(None, 30)
-        message = myfont.render('Game Over', False, (0, 0, 0))
-        self.screen.blit(message, (self.width / 2, self.height / 2))
-        pygame.display.flip()
-        while True:
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
+        if button_1.collidepoint((mx, my)):
+            if click:
+                game()
+        pygame.draw.rect(screen, (255, 0, 0), button_1)
+ 
+        click = False
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == KEYDOWN:
+                if event.key == K_ESCAPE:
+                    pygame.quit()
                     sys.exit()
-
-
-  def findScore(self):
-    
-
+            if event.type == MOUSEBUTTONDOWN:
+                if event.button == 1:
+                    click = True
+ 
+        pygame.display.update()
+        mainClock.tick(60)
+ 
+def game():
+    running = True
+    while running:
+        screen.fill((0,0,0))
+        
+        draw_text('game', font, (255, 255, 255), screen, 20, 20)
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == KEYDOWN:
+                if event.key == K_ESCAPE:
+                    running = False
+        
+        pygame.display.update()
+        mainClock.tick(60)
+ 
+def options():
+    running = True
+    while running:
+        screen.fill((0,0,0))
+ 
+        draw_text('options', font, (255, 255, 255), screen, 20, 20)
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == KEYDOWN:
+                if event.key == K_ESCAPE:
+                    running = False
+        
+        pygame.display.update()
+        mainClock.tick(60)
+ 
+main_menu()
