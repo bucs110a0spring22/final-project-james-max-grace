a15a95d7b546a74b076fa592fe8d7b968047072b
Grace Barbara <98533301+gracebarbara@users.noreply.github.com>
2022-05-10 17:00:39 +0000

    worked on player sprite

--- src/controller.py
+++ src/controller.py
@@ -13,20 +13,15 @@ class Controller:
 			:returns = None
 	   '''
 		 self.screen = pygame.display.set_mode((width, height))
-		 self.obj_layer = pygame.display.set_mode((width, height))
 		 self.clock = pygame.time.Clock()
-		 self.FPS = 60
-		 self.frame_count=0
 		 self.background= pygame.image.load('assets/CS110GameBackground.jpg').convert_alpha()
 		 self.background2 = pygame.image.load("assets/CS110GameOver.png").convert_alpha()
 		 self.background3 = pygame.image.load("assets/backgroundscreen.png").convert_alpha()
 		 self.game_state = "BEGIN"
-		 self.player_state = "RUN"
 		 self.width = width
 		 self.height = height
-		 self.white = (255,255,255)
 		 self.alive = True
-		 self.player = player.Player("karl", 10, 15, "assets/CS110Character2.png")
+		 self.player = player.Player("karl", 50, 325, "assets/CS110Character2.png")
 		 self.fallingobjects = pygame.sprite.Group()
 		 self.x = 0
 		 self.all_sprites = pygame.sprite.Group((self.player,) + tuple(self.fallingobjects))
@@ -62,7 +57,7 @@ class Controller:
 		 background_screen=pygame.display.set_mode(background_size)
 		 background_screen.blit(self.background3, background_rect)
 		 my_font = pygame.font.SysFont("impact", 30)
-		 instructions = my_font.render('Use arrows to move left and right. Avoid the falling blocks. Press space to play.', False, self.white)
+		 instructions = my_font.render('Use arrows to move left and right. Avoid the falling blocks. Press space to play.', False, (255,255,255))
 		 background_screen.blit(instructions, ((self.width / 3) - 200, self.height / 1.5))
 		 pygame.display.flip()
 		 for event in pygame.event.get():
@@ -86,8 +81,7 @@ class Controller:
 		 background_rect = self.background.get_rect()
 		 background_screen = pygame.display.set_mode(background_size)
 		 background_screen.blit(self.background, background_rect)
-		 while self.alive == True:
-			 pygame.display.flip()
+		 while self.game_state == "GAME":
 				 
 			 for event in pygame.event.get():
 				 if event.type == pygame.QUIT:
@@ -104,8 +98,11 @@ class Controller:
 				 x = random.randrange(0, 601)
 				 y = 0
 				 self.fallingobjects.add(fallingobject.FallingObject(x, y, 'assets/fallingobject.png'))
-			 self.all_sprites.draw(self.screen)
+
 			 self.fallingobjects.update()
+			 background_screen.blit(self.background, background_rect)
+			 self.all_sprites.draw(self.screen)
+			 pygame.display.flip()
 				 
 
 				
--- src/player.py
+++ src/player.py
@@ -4,16 +4,20 @@ class Player(pygame.sprite.Sprite):
 	def __init__(self, name, x, y, img_file):
 		pygame.sprite.Sprite.__init__(self)
 		self.image = pygame.image.load(img_file).convert_alpha()
-		self.rect=self.image.get_rect()
+		self.image = pygame.transform.scale(self.image, (50, 55))
+		self.rect = self.image.get_rect()
 		self.rect.x = x
 		self.rect.y = y
-		self.player_health=3
+		self.player_health = 3
 		self.speed = 5
 			
 	def move_left(self):
-		self.rect.x = self.rect.x - 1
+		self.rect.x = self.rect.x - 10
 	def move_right(self):
-		self.rect.x = self.rect.x + 1
+		self.rect.x = self.rect.x + 10
+
+	#def draw_player(self, screen):
+		#screen.blit(self.image, self.rect)
    
 
 
